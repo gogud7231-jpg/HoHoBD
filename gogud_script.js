3bfcfdce3a80996cd930a4096715e2a0ae423849
gogud7231-jpg <gogud7231@gmail.com>
2025-12-04 16:52:54 +0900

    Add files via upload

---
+++ gogud_script.js
@@ -0,0 +1,9 @@
+document.getElementById('year').textContent = new Date().getFullYear();
+
+document.getElementById('btnO').addEventListener('click', () => {
+  alert('고마워요 제 생일파티 와주실꺼죠?😊');
+});
+
+document.getElementById('btnX').addEventListener('click', () => {
+  alert('잉잉 ㅠㅠ  😭😭');
+});
